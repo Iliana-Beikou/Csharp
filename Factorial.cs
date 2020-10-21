69728d176874e7e88643e5a5f6db82e10cc4000d
Iliana-Beikou <73250228+Iliana-Beikou@users.noreply.github.com>
2020-10-21 21:46:46 +0300

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static int factorial(int n)
+        {
+            if (n == 1 ) 
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial(x));
+
+        }
+    }
+}
